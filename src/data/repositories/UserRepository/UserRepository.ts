cde5abae1166b04da8eea7362df932a7a0aba572
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-04 20:07:40 -0300

    feat: user repository

---
+++ src/data/repositories/UserRepository/UserRepository.ts
@@ -0,0 +1,123 @@
+import { IUserRepository } from "./IUserRepository";
+import { IDBAdapter } from "../../adapter/IDBAdapter";
+import { User } from "../../models/User";
+
+export class UserRepository implements IUserRepository {
+
+	constructor(private adapter: IDBAdapter<User>) { }
+
+	async store(id: string, email: string, hashPassword: string, token: string): Promise<void> {
+		await this.adapter.insert({
+			id,
+			email,
+			verification_token: token,
+			password: hashPassword
+		});
+	}
+
+	async verifyEmail(email: string): Promise<void> {
+		await this.adapter.update(
+			{ email },
+			{ verified_email: true }
+		);
+	}
+
+	async updateName(id: string, name: string): Promise<void> {
+		await this.adapter.update(
+			{ id },
+			{ name }
+		);
+	}
+
+	async destroy(id: string): Promise<void> {
+		await this.adapter.delete({ id });
+	}
+
+	async findEmailById(id: string): Promise<string> {
+		const data = await this.adapter.getOne({ id });
+
+		return data?.email;
+	}
+
+	async findEmailByEmail(email: string): Promise<string> {
+		const data = await this.adapter.getOne({ email, verified_email: true }, "AND");
+
+		return data?.email;
+	}
+
+	async findByEmailVerified(email: string): Promise<string> {
+		const data = await this.adapter.getOne({ email, verified_email: true }, "AND");
+
+		return data?.email;
+	}
+
+	async getId(email: string): Promise<string> {
+		const data = await this.adapter.getOne({ email });
+
+		return data?.id;
+	}
+
+	async getPasswordByEmail(email: string): Promise<string> {
+		const data = await this.adapter.getOne({ email });
+
+		return data?.password;
+	}
+
+	async getPasswordById(id: string): Promise<string> {
+		const data = await this.adapter.getOne({ id });
+
+		return data?.password;
+	}
+
+	async getVerificationTokenById(id: string): Promise<string> {
+		const data = await this.adapter.getOne({ id });
+
+		return data?.verification_token;
+	}
+
+	async getVerificationTokenByEmail(email: string): Promise<string> {
+		const data = await this.adapter.getOne({ email });
+
+		return data?.verification_token;
+	}
+
+	async updateVerificationTokenById(id: string, verificationToken: string): Promise<void> {
+		await this.adapter.update({ id }, { verification_token: verificationToken });
+	}
+
+	async updateVerificationTokenByEmail(email: string, verificationToken: string): Promise<void> {
+		await this.adapter.update({ email }, { verification_token: verificationToken });
+	}
+
+	async getVerificationTokenExpiryDateById(id: string): Promise<number> {
+		const data = await this.adapter.getOne({ id });
+
+		return data?.verification_token_expiry_time;
+	}
+
+	async getVerificationTokenExpiryDateByEmail(email: string): Promise<number> {
+		const data = await this.adapter.getOne({ email });
+
+		return data?.verification_token_expiry_time;
+	}
+
+	async updateVerificationTokenExpiryDateById(id: string, verificationTokenExpiryDate: number): Promise<void> {
+		await this.adapter.update({ id }, { verification_token_expiry_time: verificationTokenExpiryDate });
+	}
+
+	async updateVerificationTokenExpiryDateByEmail(email: string, verificationTokenExpiryDate: number): Promise<void> {
+		await this.adapter.update({ email }, { verification_token_expiry_time: verificationTokenExpiryDate });
+	}
+
+	async updateEmail(id: string, email: string): Promise<void> {
+		await this.adapter.update({ id }, { email });
+	}
+
+	async updatePasswordById(id: string, password: string): Promise<void> {
+		await this.adapter.update({ id }, { password });
+	}
+
+	async updatePasswordByEmail(email: string, password: string): Promise<void> {
+		await this.adapter.update({ email }, { password });
+	}
+}
