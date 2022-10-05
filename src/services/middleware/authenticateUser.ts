3bbf5b78dd87d4d5fead00e54246aeb28fbf092c
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-05 16:39:00 -0300

    feat: authenticate user middleware

---
+++ src/services/middleware/authenticateUser.ts
@@ -0,0 +1,17 @@
+import { IRequestMiddleware } from "../../core/adapter/interfaces/IRequestMiddleware";
+import { UnauthorizedError } from "../../utils/error";
+import { toolkit } from "../../utils/toolkit";
+
+export const authenticateUser = async (request: IRequestMiddleware) => {
+
+	const { authorization } = request.headers;
+
+	if (!authorization) throw new UnauthorizedError("Você não está logado");
+
+	const [, token] = authorization.split(" ");
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const decode: any = toolkit.jsonWebToken.tokenVerification(token);
+
+	return decode.id;
+};
