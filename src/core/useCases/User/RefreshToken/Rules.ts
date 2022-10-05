5ef274bbee89c73a7c4f9c16882753603a2f202a
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-05 16:22:32 -0300

    feat: use case of refresh token

---
+++ src/core/useCases/User/RefreshToken/Rules.ts
@@ -0,0 +1,24 @@
+import { MissingParamError } from "../../../../utils/error";
+import { IUserRepository } from "../../../../data/repositories/UserRepository/IUserRepository";
+import { toolkit } from "../../../../utils/toolkit";
+import { DTO } from "./DTO";
+
+export class Rules {
+
+	constructor(private repository: IUserRepository) { }
+
+	async execute({ refreshToken }: DTO) {
+		if (!refreshToken) throw new MissingParamError("O refresh token precisa ser enviado");
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+		const decode: any = toolkit.jsonWebToken.tokenVerification(refreshToken);
+
+		const email = decode?.email;
+
+		const id = await this.repository.getId(email);
+
+		const accessToken = toolkit.jsonWebToken.createToken({ id, email }, 3600);
+
+		return accessToken;
+	}
+}
