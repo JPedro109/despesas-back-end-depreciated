d9e30f1b9475e481263daa74d90c800283561ac4
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-07 19:05:44 -0300

    refactor: add doc router

--- src/services/router/index.ts
+++ src/services/router/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerJSON from "../../../swagger-docs";
 
 import user from "./user";
 import expense from "./expense";
 
 export const router = Router();
+
+router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 router.use(user);
 router.use(expense);
