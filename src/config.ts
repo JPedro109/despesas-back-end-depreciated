6953e7b4e2700f272d19527d8317983e19bc974c
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-13 17:52:42 -0300

    perf: update toolkit

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,5 @@ import { toolkit } from "./utils/toolkit";
 export const MONGO_URL = toolkit.environment.getRequiredValue("MONGO_URL");
 export const DATABASE_NAME = toolkit.environment.getRequiredValue("DATABASE_NAME");
 export const APP_URL = toolkit.environment.getRequiredValue("APP_URL");
-export const EMAIL_ORIGIN = toolkit.environment.getRequiredValue("EMAIL_ORIGIN");
 export const TOKEN_EXPIRY_TIME = toolkit.environment.getRequiredValue("TOKEN_EXPIRY_TIME");
 export const PORT = toolkit.environment.getRequiredValue("PORT") || parseInt(process.env.PORT);
