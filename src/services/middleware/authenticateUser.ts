42d75e321289352c89e457bc8d506babca63312e
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-20 18:52:59 -0300

    refactor: update type of any to JsonWebTokenModel

--- src/services/middleware/authenticateUser.ts
+++ src/services/middleware/authenticateUser.ts
@@ -1,5 +1,6 @@
 import { IRequestMiddleware } from "../../core/adapter/interfaces/IRequestMiddleware";
 import { UnauthorizedError } from "../../utils/error";
+import { JsonWebTokenModel } from "../../utils/models";
 import { toolkit } from "../../utils/toolkit";
 
 export const authenticateUser = async (request: IRequestMiddleware) => {
@@ -10,8 +11,7 @@ export const authenticateUser = async (request: IRequestMiddleware) => {
 
 	const [, token] = authorization.split(" ");
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const decode: any = toolkit.jsonWebToken.tokenVerification(token);
+	const decode: JsonWebTokenModel = toolkit.jsonWebToken.tokenVerification(token);
 
-	return decode.id;
+	return decode.id as string;
 };
