0ef48a7d50ce3fda0ce073c64f1e105777fc7ae2
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-27 18:39:41 -0300

    feat: remove redundancy

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@ export const MONGO_URL = toolkit.environment.getRequiredValue("MONGO_URL");
 export const DATABASE_NAME = toolkit.environment.getRequiredValue("DATABASE_NAME");
 export const APP_URL = toolkit.environment.getRequiredValue("APP_URL");
 export const TOKEN_EXPIRY_TIME = toolkit.environment.getRequiredValue("TOKEN_EXPIRY_TIME");
-export const PORT = toolkit.environment.getRequiredValue("PORT") || parseInt(process.env.PORT);
+export const PORT = toolkit.environment.getRequiredValue("PORT");
