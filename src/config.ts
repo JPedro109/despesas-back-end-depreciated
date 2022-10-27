7b2a0f7070164f6c6c5df4a59c8ca5483b872495
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-27 18:43:41 -0300

    refactor: center parse of environment variables

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import { toolkit } from "./utils/toolkit";
 export const MONGO_URL = toolkit.environment.getRequiredValue("MONGO_URL");
 export const DATABASE_NAME = toolkit.environment.getRequiredValue("DATABASE_NAME");
 export const APP_URL = toolkit.environment.getRequiredValue("APP_URL");
-export const TOKEN_EXPIRY_TIME = toolkit.environment.getRequiredValue("TOKEN_EXPIRY_TIME");
-export const PORT = toolkit.environment.getRequiredValue("PORT");
+export const TOKEN_EXPIRY_TIME = parseInt(toolkit.environment.getRequiredValue("TOKEN_EXPIRY_TIME"));
+export const PORT = parseInt(toolkit.environment.getRequiredValue("PORT"));
