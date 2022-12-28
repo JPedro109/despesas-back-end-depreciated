96f1ec453ac2bae6546ee51d7a9deb50339eca91
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-12-27 22:52:55 -0300

    refactor: add email validation in use case of send user email update link and your tests

--- src/core/useCases/User/SendUserEmailUpdateLink/DTO.ts
+++ src/core/useCases/User/SendUserEmailUpdateLink/DTO.ts
@@ -1,4 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../../../utils/error";
+import { toolkit } from "../../../../utils/toolkit";
 
 export class DTO {
 	constructor(public userId: string, public email: string) {
@@ -6,5 +7,9 @@ export class DTO {
 
 		if(typeof userId !== "string" || typeof email !== "string") 
 			throw new InvalidParamError("Os tipos dos campos estão incorretos");
+
+		const emailValid = toolkit.validation.email(email);
+
+		if(!emailValid) throw new InvalidParamError("Coloque um e-mail válido");
 	}
 }
